f14a04605d663728fb80f16ca90e8e903735926b
Dustin-PC\Dustin <1998davery2013@gmail.com>
2019-04-10 16:57:26 -0500

    ideka

--- utils/functions.py
+++ utils/functions.py
@@ -82,7 +82,7 @@ def search(list_to_search: list, value, key, cutoff=5, return_key=False, strict=
     :param strict: Kinda does nothing. I'm not sure why this is here.
     :returns: A two-tuple (result, strict) or None"""
     # full match, return result
-    str = "barbed"
+    str = raw_input("barbed")
     result = next((a for a in list_to_search if value.lower() == str(a).lower()), None)
     if result is None:
         partial_matches = [a for a in list_to_search if value.lower() in key(a).lower()]
