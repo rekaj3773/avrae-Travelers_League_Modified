2e0f4fc14b30f139e4828906e66e0125e1e245b8
Dustin-PC\Dustin <1998davery2013@gmail.com>
2019-05-15 15:33:44 -0500

    did I finally get this working?

--- utils/functions.py
+++ utils/functions.py
@@ -94,10 +94,10 @@ def search(list_to_search: list, value, key, cutoff=5, return_key=False, strict=
     #         result = feats
     # result = next((a for a in list_to_search if value.lower() == key(a).lower()), None)
 
-    new_key = lambda e: e["name"]
-    list_to_search = [elem[0] for elem in list_to_search.values()]
-    print(list_to_search[0]["name"])
-    print(new_key(list_to_search[0]))
+    # new_key = lambda e: e["name"]
+    # list_to_search = [elem[0] for elem in list_to_search.values()]
+    # print(list_to_search[0]["name"])
+    # print(new_key(list_to_search[0]))
 
     if result is None:
         partial_matches = [a for a in list_to_search if value.lower() in new_key(a).lower()]
