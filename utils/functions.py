d52497c19930781daa483d649d6a262274156f49
Dustin-PC\Dustin <1998davery2013@gmail.com>
2019-05-15 14:32:47 -0500

    HOW DOES THIS BOT WORK???

--- utils/functions.py
+++ utils/functions.py
@@ -93,6 +93,7 @@ def search(list_to_search: list, value, key, cutoff=5, return_key=False, strict=
     #     if value.lower == key(feat).lower():
     #         result = feats
     # result = next((a for a in list_to_search if value.lower() == key(a).lower()), None)
+    print(type(list_to_search))
     print(list_to_search)
     if result is None:
         partial_matches = [a for a in list_to_search if value.lower() in key(a).lower()]
