bdb03e0ac1e8b22ad4c426a4742c1a3c100d1f67
Dustin-PC\Dustin <1998davery2013@gmail.com>
2019-06-06 01:28:08 -0500

    Typos

--- cogsmisc/points.py
+++ cogsmisc/points.py
@@ -117,8 +117,7 @@ class Points(commands.Cog):
             except KeyError:
                 continue
             renown_str = await self.getPointTotalString(ctx, document["points"])
-            role = get(ctx.guild.emojis,id=role)
-            string_input = role_game_name.__str__()
+            string_input = role.__str__()
             string_input = string_input.split("-",1)[1]
             try:
                 string_input = document["emoji"] + " " + string_input
